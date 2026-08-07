6d01871ad1d29c938083a37f448f4ba6518c7590
duduamora2005-netizen <duduamora2005@gmail.com>
2026-08-07 16:02:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 from flask import Flask, render_template
-from scanner_put import rodar_scanner
+import scanner_put
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # Executa a função do scanner
-    acoes = rodar_scanner()
-    
-    # Pega o ticker do ativo com maior Score
-    melhor_opcao = acoes[0]["Ativo"].replace(".SA", "") if acoes else "N/A"
-    
-    return render_template(
-        'index.html', 
-        acoes=acoes, 
-        total=len(acoes), 
-        melhor=melhor_opcao
-    )
+def home():
+    # Executa a busca de opções do seu script scanner_put.py
+    dados = scanner_put.executar_scanner() 
+    return render_template('index.html', dados=dados)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
